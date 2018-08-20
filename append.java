9e52af1bea47759cf569e2904563f78054a1d689
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-20 15:44:03 +0530

    Create append.java

---
+++ append.java
@@ -0,0 +1,19 @@
+package janani;
+import java.util.Scanner;
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str;
+		int i;
+		System.out.println("enter the String");
+		Scanner ja=new Scanner(System.in);
+		str=ja.nextLine();
+		StringBuilder a=new StringBuilder(str);
+      a.append(".");
+		System.out.println(a);
+		}
+
+	}
+
+
